d286cb2090e234e84b292c79a18b420298f03cd5
helionux <deouleallahh@gmail.com>
2026-01-20 03:52:28 +0100

    Hero, add image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,7 @@
 import { Bed, Camera, House, Search, UtensilsCrossed } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
-
+import caption from '../../../public/caption.jpg'
 export default function Hero() {
   const subLinks = [
     {
@@ -9,12 +10,12 @@ export default function Hero() {
       icon: <House />
     },
     {
-      name: "Hotels",
+      name: "Hôtels",
       path: "/hotel",
       icon: <Bed />
     },
     {
-      name: "Activites",
+      name: "Activités",
       path: "/activite",
       icon: <Camera />
     },
@@ -26,25 +27,36 @@ export default function Hero() {
   ]
   return (
       <section className="max-w-7xl mx-auto px-6 lg:px-10">
-          <div className="flex flex-col flex-wrap items-center justify-center ">
-              <div>
-                  <h1 className="text-center text-[54px] leading-12 md:text-6xl md:leading-17.5 mt-15 font-semibold max-w-3xl font-lexend text-[#002b11]">Quelle destination ?</h1>
-              </div>
-              <div className="flex flex-col">
-                <nav className="flex flex-row gap-7 mt-12 text-[#002b11] font-lexend font-bold">
+        <div className="flex flex-col flex-wrap items-center justify-center ">
+        <div>
+          <h1 className="text-center text-[54px] leading-12 md:text-6xl md:leading-17.5 mt-15 font-semibold max-w-3xl font-lexend text-[#002b11]">Quelle destination ?</h1>
+        </div>
+        
+        <div className="flex flex-col">
+          <nav className="flex flex-row gap-7 mt-12 text-[#002b11] font-lexend font-bold">
                   {
                     subLinks.map((link, idx) => (
                       <Link href={link.path} key={idx} className="flex items-center gap-1.5 text-[#002b11] border-b-2 border-transparent hover:border-[#002b11] transition-colors duration-300 pb-2">{link.icon}{link.name}</Link>
                     ))
                   }
           </nav>
-          <div className=" max-w-7xl flex items-center mt-5 py-3 border pl-4 gap-2 bg-white border-gray-500/30 h-11.5 rounded-full overflow-hidden w-full">
-            <Search />
-            <input type="text" className="w-full  h-full outline-none text-sm text-gray-500" />
-            <button type="submit" className="bg-[#00eb5a] w-32 h-9 rounded-full text-sm text-black font-bold  mr-1.5">Search</button>
         </div>
-              </div>
+            <div className="w-3/4  flex items-center mt-5  border pl-4 gap-2 bg-white border-gray-500/30 h-14 rounded-full overflow-hidden ">
+                <Search />
+                <input type="text" className="w-full h-full outline-none text-[16px] text-gray" placeholder="Des endroits a visiter,des activiés, des hôtels..."/>
+                <button type="submit" className="bg-[#00eb5a] w-32 px-4 py-2.5 border-black border rounded-full text-[16px] text-black font-bold  mr-1.5">Search</button>
         </div>
+
+        <div className="w-full mx-auto grid grid-cols-2 gap-6 p-4 bg-[#00eb5a] rounded-2xl mt-20">
+          <div className="w-full max-w-full"> 
+            <Image src={caption} alt="Caption" width={100} height={100} className="w-full object-fit rounded-2xl"/>
+          </div>
+          
+          <div className="border items-center justify-center">
+            <h1 className="text-center text-[54px] tracking-normal leading-12 md:text-6xl md:leading-17.5 mt-15 font-semibold max-w-3xl font-lexend text-[#002b11]">Trouvez des activités selon vos envies</h1>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
